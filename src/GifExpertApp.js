1f30dd3b5d5740c49e9e7c83ccb5d20e1ed9382e
Aaron Otero <aaronoterolavi@gmail.com>
2022-03-10 00:26:53 -0500

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+const GifExpertApp = ( ) => {
+
+    
+    const [categories, setcategories] = useState(['One Punch']);
+
+    // const handleAdd = ()=>{
+    //     setcategories([...categories,'Naruto']);
+    // }
+
+
+    return (
+        <>
+            <h2>  GifExpertApp </h2>
+            <AddCategory setcategories={setcategories}/>
+            <hr/>
+
+            <ol>
+                { 
+                    categories.map( category =>(
+                            <GifGrid 
+                                key={category}
+                                category={category}
+                            />
+                    ))
+                }
+            </ol>
+        </>
+    );
+        
+} 
+
+export default GifExpertApp;
